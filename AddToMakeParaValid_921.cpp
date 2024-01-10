e837edc880827d0daf337caf85dfde50a3c1ca01
Abhinav Tiwari <136241502+abhinavtiwari1729@users.noreply.github.com>
2024-01-11 00:40:04 +0530

    Create AddToMakeParaValid_921.cpp

---
+++ AddToMakeParaValid_921.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+       
+       int min_add = 0;
+       stack<char> st;
+       for(char ch : s) {
+            if(ch == '(')
+                st.push(ch);
+            if(ch == ')'){
+                if (!st.empty())
+                    st.pop();
+                else 
+                    min_add++;
+            } 
+
+       }
+
+        min_add += st.size();
+        return min_add;
+
+    }
+};
