68ed3e4e176706c7eec3dca760cd54db4fa62017
Abhinav Tiwari <136241502+abhinavtiwari1729@users.noreply.github.com>
2024-01-21 00:46:39 +0530

    Create WildCardMatchingDPMem_44.cpp

---
+++ WildCardMatchingDPMem_44.cpp
@@ -0,0 +1,41 @@
+class Solution {
+
+    bool RecursiveMatch(string & s , string & p , int s_p , int p_p , vector<vector<int>> &dp ) {
+
+        if (s_p < 0 && p_p < 0)
+            return true;
+        
+        if (p_p < 0 && s_p >= 0)
+            return false;
+        
+        if (s_p < 0 && p_p >= 0) {
+            for(int p_sp = p_p ; p_sp >= 0 ; p_sp--)
+                if(p[p_sp] != '*')
+                    return false;
+            return true;
+        }
+
+        if(dp[s_p][p_p] != -1)
+            return dp[s_p][p_p];
+
+        if (s[s_p] == p[p_p] || p[p_p] == '?')
+            return dp[s_p][p_p] = RecursiveMatch(s , p , s_p-1 , p_p-1 , dp);
+        
+        if (p[p_p] == '*')
+            return dp[s_p][p_p] = RecursiveMatch(s , p , s_p-1 , p_p , dp) || RecursiveMatch(s , p , s_p , p_p-1 , dp);
+        
+        return false;
+
+
+
+    }
+
+
+public:
+    bool isMatch(string s, string p) {
+
+        vector<vector<int>> dp(s.size() , vector<int>(p.size() , -1));
+        return RecursiveMatch(s , p , s.size() - 1 , p.size() - 1 , dp );
+
+    }
+};
