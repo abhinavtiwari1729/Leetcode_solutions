20b02c2d73b2fe900d6996f91129db069611edfb
Abhinav Tiwari <136241502+abhinavtiwari1729@users.noreply.github.com>
2024-02-26 14:34:45 +0530

    Update BestTimeTobuyAndSell_121.cpp

--- BestTimeTobuyAndSell_121.cpp
+++ BestTimeTobuyAndSell_121.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-                int size = prices.size();
-        if (size == 0 || size == 1)
-            return 0;
-        int max = prices[size-1];
+
+        int min = prices[0];
         int max_profit = 0;
-        int profit = 0;
-        
-        for (int i = size-2  ; i > -1 ; i--) {
-            int item = prices[i];
-            if(item > max)
-                max = item;
-            profit = max - item ; 
-            
-            if (profit > max_profit )
-                max_profit = profit;
+
+        for(int i = 1 ; i < prices.size() ; i++){
+
+            int profit = prices[i] - min ;
+            max_profit = max(max_profit , profit);
+
+            min = std::min(min , prices[i]);
+
         }
-        
+
         return max_profit;
     }
 };
